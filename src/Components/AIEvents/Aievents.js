486220bf73c725338043013076040818cd790c53
Dipak1611 <dipak.adiance@gmail.com>
2024-08-12 18:44:41 +0530

    done

--- src/Components/AIEvents/Aievents.js
+++ src/Components/AIEvents/Aievents.js
@@ -3,20 +3,22 @@ import styles from "../Aievents.module.css";
 
 export default function Aievents() {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectedDateIndex, setSelectedDateIndex] = useState();
 
   const previousWeek = () => {
     const newDate = new Date(currentDate);
     newDate.setDate(newDate.getDate() - 7);
     setCurrentDate(newDate);
-    console.log(currentDate);
+    setSelectedDateIndex(null); // Reset selection when changing weeks
   };
 
   const nextWeek = () => {
     const newDate = new Date(currentDate);
     newDate.setDate(newDate.getDate() + 7);
     setCurrentDate(newDate);
-    console.log(currentDate);
+    setSelectedDateIndex(null); // Reset selection when changing weeks
   };
+
   const getDayName = (date) => {
     const daysOfWeek = [
       "Sunday",
@@ -29,6 +31,7 @@ export default function Aievents() {
     ];
     return daysOfWeek[date];
   };
+
   const getMonthName = (date) => {
     const monthsOfYear = [
       "January",
@@ -38,7 +41,7 @@ export default function Aievents() {
       "May",
       "June",
       "July",
-      "Aug",
+      "August", // Fixed "Aug" to "August"
       "September",
       "October",
       "November",
@@ -47,17 +50,33 @@ export default function Aievents() {
     return monthsOfYear[date];
   };
 
+  const handleDateClick = (index, date) => {
+    setSelectedDateIndex(index);
+
+    console.log(`Selected date: ${date.getDate()} ${getMonthName(date.getMonth())} ${getDayName(date.getDay())}`);
+  };
+
   const renderDates = () => {
     const dates = [];
     for (let i = 0; i < 7; i++) {
       const date = new Date(currentDate);
       date.setDate(currentDate.getDate() + i);
       dates.push(
-        <div key={i} className={styles[22]}>
-          <div className={styles.jul}>
-            {date.getDate()} {getMonthName(date.getMonth())}
+        <div
+          key={i}
+          className={`${styles.div30} ${
+            selectedDateIndex === i ? styles.selected : ""
+          }`}
+          onClick={() => handleDateClick(i, date)}
+        >
+          <div className={styles.div31}>
+            <div className={styles.div32}>
+              <div className={styles.jul}>
+                {date.getDate()} {getMonthName(date.getMonth())}
+              </div>
+              <div className={styles.sun}>{getDayName(date.getDay())}</div>
+            </div>
           </div>
-          <div className={styles.sun}>{getDayName(date.getDay())}</div>
         </div>
       );
     }
@@ -68,51 +87,10 @@ export default function Aievents() {
     <>
       <div className={styles.eventsDesktop}>
         <div className={styles.div}>
-          <div className={styles.div2}>
-            {/* <div className={styles.button}>
-              <div className={styles.buttonBase}>+ device</div>
-            </div> */}
-          </div>
+          <div className={styles.div2}></div>
         </div>
         <div className={styles.div3}>
           <div className={styles.div4}>
-            {/* <div className={styles.column}>
-              <div className={styles.div5}>
-                <div className={styles.div6} />
-                <div className={styles.div7}>
-
-                  <div className={styles.cameras}>Cameras</div>
-                </div>
-                <div className={styles.div8}>
-
-                  <div className={styles.multiScreen}>Multi Screen</div>
-                </div>
-                <div className={styles.div9}>
-
-                  <div className={styles.events}>Events</div>
-                </div>
-                <div className={styles.div10}>
-
-                  <div className={styles.profile}>Profile</div>
-                </div>
-                <div className={styles.div11}>
-
-                  <div className={styles.subscription}>Subscription</div>
-                </div>
-                <div className={styles.div12}>
-
-                  <div className={styles.settings}>Settings</div>
-                </div>
-                <div className={styles.div13}>
-
-                  <div className={styles.openMap}>Open Map</div>
-                </div>
-                <div className={styles.div14}>
-
-                  <div className={styles.logout}>Logout</div>
-                </div>
-              </div>
-            </div> */}
             <div className={styles.column2}>
               <div className={styles.div15}>
                 <div className={styles.aiEvents}>AI Events</div>
@@ -120,37 +98,7 @@ export default function Aievents() {
                   <div className={styles.listOfEventsTriggeredByCameras}>
                     List of events triggered by cameras
                   </div>
-                  <div className={styles.div17}>
-                    <div className={styles.div18}>
-                      <div className={styles.div19}>
-                        <div className={styles.div20}>
-                          <div className={styles.jul}>
-                            {renderDates()[0]}
-                            {/* {currentDate.getDate()}{" "}
-                            {getMonthName(currentDate.getMonth())}
-                          </div>
-                          <div className={styles.sun}>
-                            {getDayName(currentDate.getDay())} */}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles.div21}>
-                      <div className={styles.div22}>
-                        <div className={styles.div23}>
-                          <div className={styles.jul}>
-                            {renderDates()[1]}
-                            {/* {currentDate.getDate() + 1}{" "}
-
-                            {getMonthName(currentDate.getMonth())}
-                          </div>
-                          <div className={styles.sun}>
-                            {getDayName(currentDate.getDay() + 1)} */}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <div className={styles.div17}>{renderDates()}</div>
                 </div>
               </div>
             </div>
@@ -185,84 +133,12 @@ export default function Aievents() {
                             previous week
                           </div>
                         </div>
-                        <div className={styles.div29}>
-                          <div className={styles.div30}>
-                            <div className={styles.div31}>
-                              <div className={styles.div32}>
-                                <div className={styles.jul}>
-                                  {renderDates()[2]}
-                                  {/* {currentDate.getDate() + 2}{" "}
-                                  {getMonthName(currentDate.getMonth())}
-                                </div>
-                                <div className={styles.sun}>
-                                  {getDayName(currentDate.getDay() + 2)} */}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={styles.div33}>
-                            <div className={styles.div34}>
-                              <div className={styles.div35}>
-                                <div className={styles.jul}>
-                                  {renderDates()[3]}
-                                  {/* {currentDate.getDate() + 3}{" "}
-                                  {getMonthName(currentDate.getMonth())}
-                                </div>
-                                <div className={styles.sun}>
-                                  {getDayName(currentDate.getDay() + 3)} */}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={styles.div36}>
-                            <div className={styles.div37}>
-                              <div className={styles.div38}>
-                                <div className={styles.jul}>
-                                  {renderDates()[4]}
-                                  {/* {currentDate.getDate() + 4}{" "}
-                                  {getMonthName(currentDate.getMonth())}
-                                </div>
-                                <div className={styles.sun}>
-                                  {getDayName(currentDate.getDay() + 4)} */}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={styles.div39}>
-                            <div className={styles.div40}>
-                              <div className={styles.div41}>
-                                <div className={styles.jul}>
-                                  {renderDates()[5]}
-                                  {/* {currentDate.getDate() + 5}{" "}
-                                  {getMonthName(currentDate.getMonth())}
-                                </div>
-                                <div className={styles.sun}>
-                                  {getDayName(currentDate.getDay() + 5)} */}
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                     <div className={styles.column5}>
                       <div className={styles.div42}>
                         <div className={styles.nextWeek} onClick={nextWeek}>
                           next week
-                        </div>
-                        <div className={styles.div43}>
-                          <div className={styles.div44}>
-                            <div className={styles.div45}>
-                              <div className={styles.jul}>
-                                {renderDates()[6]}
-                                {/* {currentDate.getDate() + 6}{" "}
-                                {getMonthName(currentDate.getMonth())}
-                              </div>
-                              <div className={styles.sun}>
-                                {getDayName(currentDate.getDay() + 6)} */}
-                              </div>
-                            </div>
-                          </div>
                         </div>
                       </div>
                     </div>
@@ -281,7 +157,6 @@ export default function Aievents() {
               <option value="model4">Detection Model 4</option>
               <option value="model5">Detection Model 5</option>
             </select>
-
           </div>
           <div className={styles.clear}>clear</div>
         </div>
